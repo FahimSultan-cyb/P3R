bcaf525c11c718fb7d3eb8a89c5365d6b45c2103
Md. Fahim Sultan <mdfahimsultan@oakland.edu>
2025-09-17 08:37:23 -0400

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,10 @@
-from .p3r_model import P3RHeadGateModel
+from .p3r_headgate import P3RHeadGateModel
 from .components import CompactPromptPool, CompactRouterMLP, CompactHeadGate, CompactSymbolicClassifier
 
-__all__ = ['P3RHeadGateModel', 'CompactPromptPool', 'CompactRouterMLP', 'CompactHeadGate', 'CompactSymbolicClassifier']
+__all__ = [
+    'P3RHeadGateModel',
+    'CompactPromptPool',
+    'CompactRouterMLP',
+    'CompactHeadGate',
+    'CompactSymbolicClassifier'
+]
