ad91c6af85d16ef5b7870cd23c6a80c7e684cd28
Md. Fahim Sultan <mdfahimsultan@oakland.edu>
2025-09-23 20:39:51 -0400

    Update setup.py

--- P3R_peft/setup.py
+++ P3R_peft/setup.py
@@ -11,7 +11,7 @@ setup(
     description="Parameter-Efficient Fine-tuning for Code Language Models using P3R and HeadGate",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FahimSultan-cyb/P3R",
+    url="https://github.com/FahimSultan-cyb/P3R/P#R_peft",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
